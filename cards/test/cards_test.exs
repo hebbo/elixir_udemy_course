8b47922fa5620d3859032cd327014ecd1c053470
Angel Ruiz <angel@cultureamp.com>
2017-03-04 12:00:59 +1100

    First commit

---
+++ cards/test/cards_test.exs
@@ -0,0 +1,10 @@
+defmodule CardsTest do
+  use ExUnit.Case
+  doctest Cards
+
+
+  test "deck creates a deck with 20 cards" do
+    deck = Cards.create_deck
+    assert length(deck) == 20
+  end
+end
